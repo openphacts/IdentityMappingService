d68f9f8fb0b73548945a91c118c45717c90f47aa
Christian-B <brenninc@cs.man.ac.uk>
2013-08-27 10:53:46 +0100

    removed validator specific options

--- loader/src/uk/ac/manchester/cs/openphacts/ims/loader/RdfFactoryIMS.java
+++ loader/src/uk/ac/manchester/cs/openphacts/ims/loader/RdfFactoryIMS.java
@@ -18,11 +18,6 @@ public class RdfFactoryIMS {
 
     public static RdfReader getReader() throws BridgeDBException {
         try {
-            RdfFactory.setStandAloneValidator(false);
-        } catch (VoidValidatorException ex) {
-            throw new BridgeDBException ("Unexpceted call to RdfFactory before this call." + ex);
-        }
-        try {
             if (ConfigReader.inTestMode()){
                 return RdfFactory.getTestFilebase();
             } else {
