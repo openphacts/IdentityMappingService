c5ebecd090b30bef1215fea5da9ef9675e6e3a49
Christian-B <brenninc@cs.man.ac.uk>
2013-10-07 14:14:28 +0100

    Updated to version 1.3

--- loader/src/uk/ac/manchester/cs/openphacts/ims/loader/RunLoader.java
+++ loader/src/uk/ac/manchester/cs/openphacts/ims/loader/RunLoader.java
@@ -181,7 +181,7 @@ public class RunLoader {
             //URL url;
             if (argv.length == 0){
                 //url = new URL("file:///C:/Dropbox/ims/dev/version1.3.alpha7/load.xml");
-                urlReader = new UrlReader("http://openphacts.cs.man.ac.uk/ims/dev/version1.3.alpha7/load.xml");
+                urlReader = new UrlReader("http://openphacts.cs.man.ac.uk/ims/linkset/version1.3/load.xml");
            } else {
                 //url = null;
                 urlReader = new UrlReader(argv[0]);
