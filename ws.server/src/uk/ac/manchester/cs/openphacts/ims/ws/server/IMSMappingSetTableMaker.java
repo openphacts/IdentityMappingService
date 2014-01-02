4b0f32c81cdcb3b69bdfc36bf09ae31ca770076a
Christian-B <brenninc@cs.man.ac.uk>
2014-01-02 12:31:36 +0000

    updated bridgedb package names

--- ws.server/src/uk/ac/manchester/cs/openphacts/ims/ws/server/IMSMappingSetTableMaker.java
+++ ws.server/src/uk/ac/manchester/cs/openphacts/ims/ws/server/IMSMappingSetTableMaker.java
@@ -7,11 +7,10 @@ package uk.ac.manchester.cs.openphacts.ims.ws.server;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.util.List;
-import java.util.logging.Level;
 import javax.servlet.http.HttpServletRequest;
 import org.bridgedb.statistics.MappingSetInfo;
-import org.bridgedb.uri.ws.server.MappingSetTableMaker;
 import org.bridgedb.utils.BridgeDBException;
+import org.bridgedb.ws.uri.MappingSetTableMaker;
 import org.openrdf.model.impl.URIImpl;
 import uk.ac.manchester.cs.datadesc.validator.ws.WsValidationConstants;
 
