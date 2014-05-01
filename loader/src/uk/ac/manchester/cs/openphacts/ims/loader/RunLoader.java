131f68c0535faab762d91fa1f408998457cf57fb
Christian-B <brenninc@cs.man.ac.uk>
2014-05-01 09:28:55 +0100

    Updated for version 1.4.1

--- loader/src/uk/ac/manchester/cs/openphacts/ims/loader/RunLoader.java
+++ loader/src/uk/ac/manchester/cs/openphacts/ims/loader/RunLoader.java
@@ -163,6 +163,7 @@ public class RunLoader {
                     } else if (link.endsWith("/")){
                         loadDirectory(address + link);
                     } else {
+                        System.out.println(loaded);
                         loadLinkset(address, link);
                     }
                 }
@@ -200,8 +201,8 @@ public class RunLoader {
             UrlReader urlReader;
             //URL url;
             if (argv.length == 0){
-                urlReader = new UrlReader("file:///C:/Dropbox/ims/linkset/versions1.4.1/load.xml");
-                //urlReader = new UrlReader("http://openphacts.cs.man.ac.uk/ims/dev/version1.3.1.alpha1/load.xml");
+                //urlReader = new UrlReader("file:///C:/Dropbox/ims/linkset/versions1.4.1/load.xml");
+                urlReader = new UrlReader("http://openphacts.cs.man.ac.uk/ims/linkset/version1.4.1/load.xml");
            } else {
                 //url = null;
                 urlReader = new UrlReader(argv[0]);
